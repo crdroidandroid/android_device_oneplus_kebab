93ee46cf37d4f664919e893a1199d2b1abfcfd7e
firebird11 <hbgassel@gmail.com>
2021-02-01 19:48:02 +0200

    kebab: use Coral fingerprint
    Change-Id: I48b144f788dd78203f88f76418614b93d5e7ca5b

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -35,6 +35,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8T \
     PRODUCT_NAME=OnePlus8T_EEA \
-    PRIVATE_BUILD_DESC="OnePlus8T-user 11   release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus8T_EEA/OnePlus8T:11/RP1A.201005.001/2011101425:user/release-keys
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys
