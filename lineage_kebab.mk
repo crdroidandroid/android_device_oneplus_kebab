5a04a702aa13f39686aa9cb51a9109fff8d64772
firebird11 <hbgassel@gmail.com>
2021-05-21 14:37:54 +0300

    kebab: Use May fingerprint from Redfin
    Change-Id: Ie930943db34c61d940e4506cabdb2aecead04f4b

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -37,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8T_EEA \
     PRIVATE_BUILD_DESC="OnePlus8T-user 11   release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus8T_EEA/OnePlus8T:11/RP1A.201005.001/2011101425:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
