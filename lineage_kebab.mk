6bc987d970a459f6878971804fdc2e41f10d48a7
firebird11 <hbgassel@gmail.com>
2021-07-13 03:55:57 +0300

    kebab: Update to redfin July 2021 fingerprint
    Change-Id: Id78926d3c5f6860a6d02a47e8320f10d8eded9ef

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -37,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8T_EEA \
     PRIVATE_BUILD_DESC="OnePlus8T-user 11   release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
