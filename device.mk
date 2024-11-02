3c41cce7c1af9c914c27d7382bc37daba80143da
bojantanasic <bojan.tanasic@gmail.com>
2024-11-02 09:38:34 +0100

    Revert "Merge pull request #1 from crdroidandroid/15.0"
    This reverts commit 2b94d30363673abb8e2b67122b1de4b3afad40b8, reversing
    changes made to 8ff8a9345c99de4951ca6063c32465f0659f260e.

--- device.mk
+++ device.mk
@@ -27,9 +27,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk
 
-# LiveDisplay
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
